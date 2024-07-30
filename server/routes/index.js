5431937a23ec8f9ff12ad107b727dcf17b3ff902
John Lopez <john.lopez3695@gmail.com>
2024-07-30 16:15:52 -0400

    created routes for jobs

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import auth from "./auth.js"
+import jobs from "./jobs.js"
 
 const router = Router()
 
 router.use("/auth", auth);
+router.use("/jobs", jobs);
 
 export default router;
---
+++ server/routes/jobs.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import * as controllers from "../controllers/jobs.js"
+
+const router = Router();
+
+//////////////////  ROUTES  ////////////////////////
+
+router.get("/listings", controllers.getJobs);
+router.get("/:id", controllers.getJob);
+router.get("/title/:title", controllers.getJobsByTitle);
+router.get("/category/:category", controllers.getJobsByCategory);
+router.get("/location/:location", controllers.getJobsByLocation);
+router.get("/company/:company", controllers.getJobsByCompany);
+router.get("/workSetting/:remote", controllers.getJobsBySetting);
+router.get("/employment/:employmentType", controllers.getJobsByEmploymentType);
+router.get("/experienceLevel/:experienceLevel", controllers.getJobsByExperience)
+
+export default router;
