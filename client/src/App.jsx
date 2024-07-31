34e1c7983ede49e83159e7193fdbd3fecd322802
John Lopez <john.lopez3695@gmail.com>
2024-07-31 10:08:37 -0400

    added verification and logout functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,34 @@
 import './App.css'
+import { useState, useEffect } from 'react';
+import { Routes, Route } from "react-router-dom";
+import NavBar from './components/Nav/Nav.jsx';
+import Landing from './pages/Landing/Landing.jsx';
+import Register from './pages/Auth/Register/Register.jsx';
+import Login from './pages/Auth/Login/Login.jsx';
+import { verify } from './services/users.js';
 
 function App() {
+
+  const [user,setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await verify();
+      response ? setUser(response) : setUser(null)
+    };
+
+    fetchUser()
+  },[]);
+
   return (
     <>
-      <h2>Welcome to JobList</h2>
+      <NavBar user={user} setUser={setUser}/>
+      <Routes> 
+        <Route path='/' element={ <Landing />}/>
+        <Route path='/register' element={ <Register setUser={setUser} /> }/>
+        <Route path='/login' element={ <Login setUser={setUser} /> }/>
+      </Routes> 
+
     </>
   )
 }
--- client/src/services/users.js
+++ client/src/services/users.js
@@ -47,4 +47,41 @@ export const login = async (credentials) => {
         // Optionally, throw a new error with the transformed message or handle it differently
         throw new Error(errorMessage);
     }
-}
+};
+
+export const logout = async () => {
+    try {
+        if (localStorage.getItem("token") === null) {
+            throw new Error("No token found");
+        }
+
+        //remove token from local storage
+        localStorage.removeItem("token");
+
+        return true;
+    } catch (error) {
+        // Log the error for debugging purposes
+        console.error("log out error:", error);
+
+        // Transform the error into a user-friendly message or custom error object
+        const errorMessage =
+        error.response?.data?.message || "Logging out failed due to an unexpected error.";
+
+        // Optionally, throw a new error with the transformed message or handle it differently
+        throw new Error(errorMessage);
+    }
+};
+
+export const verify = async () => {
+    //error handling
+    if (localStorage.getItem("token") === null) {
+        throw new Error("No token found");
+    }
+
+    //get token from local storage
+    const token = localStorage.getItem("token");
+    if (token) {
+        const res = await api.get("auth/verify");
+    return res.data;
+    }
+};
