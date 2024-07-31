27c58c8546f8876703989b81a9892c29058a976c
John Lopez <john.lopez3695@gmail.com>
2024-07-31 19:18:05 -0400

    added and modified routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,5 +8,7 @@ const router = Router();
 router.post("/register", controllers.registerUser)
 router.post("/login", controllers.loginUser)
 router.get('/verify',controllers.verifyUser)
+router.post('/apply/:jobId', controllers.applyForJob)
+router.get('/jobsApplied', controllers.getAppliedJobs)
 
 export default router
--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -7,12 +7,16 @@ const router = Router();
 
 router.get("/listings", controllers.getJobs);
 router.get("/:id", controllers.getJob);
-router.get("/title/:title", controllers.getJobsByTitle);
-router.get("/category/:category", controllers.getJobsByCategory);
-router.get("/location/:location", controllers.getJobsByLocation);
-router.get("/company/:company", controllers.getJobsByCompany);
-router.get("/workSetting/:remote", controllers.getJobsBySetting);
-router.get("/employment/:employmentType", controllers.getJobsByEmploymentType);
-router.get("/experienceLevel/:experienceLevel", controllers.getJobsByExperience)
+router.get("/search/listings", controllers.searchJobs);
+
+////////// SEARCHING SPECIFIC FIELDS FOR DEGGUBING ///////
+
+// router.get("/title/:title", controllers.getJobsByTitle);
+// router.get("/category/:category", controllers.getJobsByCategory);
+// router.get("/location/:location", controllers.getJobsByLocation);
+// router.get("/company/:company", controllers.getJobsByCompany);
+// router.get("/workSetting/:remote", controllers.getJobsBySetting);
+// router.get("/employment/:employmentType", controllers.getJobsByEmploymentType);
+// router.get("/experienceLevel/:experienceLevel", controllers.getJobsByExperience)
 
 export default router;
